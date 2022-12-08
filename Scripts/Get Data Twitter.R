c01f7345bbdef3606b902170220a9b6054763314
federico00taddei <icotaddei@gmail.com>
2022-12-08 19:54:20 +0100

    IDS twitter farright parties italy

--- Scripts/Get Data Twitter.R
+++ Scripts/Get Data Twitter.R
@@ -1,8 +1,30 @@
 # First try get Twitter info
 auth_as('taddo')
+library(tidyverse)
+library(dplyr)
+library(rtweet)
+# Get the list of Far Right Parties
+
+far_right_parties = lists_members(
+  list_id = "1600600462612434969",
+  slug = NULL,
+  owner_user = NULL,
+  n = 100,
+  cursor = "-1",
+  token = NULL,
+  retryonratelimit = NULL,
+  verbose = TRUE,
+  parse = TRUE,
+)
+
+
+ids_far_right_parties = tibble(far_right_parties$user_id)
+
+
+# Get the list of Far Right Culture
 
 id_far_right_parties = lists_members(
-  list_id = "1600600462612434969",
+  list_id = "",
   slug = NULL,
   owner_user = NULL,
   n = 100,
