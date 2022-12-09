94598d3d1152c360d08c6a66eba07f431de2ad7a
federico00taddei <icotaddei@gmail.com>
2022-12-09 13:40:12 +0100

    Farright assvcoiations, right-wing parties and associations

--- Scripts/Get Data Twitter.R
+++ Scripts/Get Data Twitter.R
@@ -23,8 +23,8 @@ ids_far_right_parties = tibble(far_right_parties$user_id)
 
 # Get the list of Far Right Culture
 
-id_far_right_parties = lists_members(
-  list_id = "",
+far_right_culture = lists_members(
+  list_id = "1600967243881451521",
   slug = NULL,
   owner_user = NULL,
   n = 100,
@@ -34,3 +34,39 @@ id_far_right_parties = lists_members(
   verbose = TRUE,
   parse = TRUE,
 )
+
+ids_far_right_culture = tibble(far_right_culture$user_id)
+
+# Get the list of Right-wing Parties
+
+right_wing_parties = lists_members(
+  list_id = "1601142337874395136",
+  slug = NULL,
+  owner_user = NULL,
+  n = 100,
+  cursor = "-1",
+  token = NULL,
+  retryonratelimit = NULL,
+  verbose = TRUE,
+  parse = TRUE,
+)
+
+
+ids_right_wing_parties = tibble(right_wing_parties$user_id)
+
+# Get the list of Right-wing Culture
+
+right_wing_culture = lists_members(
+  list_id = "1601188241394237442",
+  slug = NULL,
+  owner_user = NULL,
+  n = 100,
+  cursor = "-1",
+  token = NULL,
+  retryonratelimit = NULL,
+  verbose = TRUE,
+  parse = TRUE,
+)
+
+ids_right_wing_culture = tibble(right_wing_culture$user_id)
+
