67bd7c8cbebaeb5d6abbd21e7b7c883e0c6db6b6
federico00taddei <icotaddei@gmail.com>
2022-12-12 19:25:42 +0100

    Exporting raw follower dataframe

--- Scripts/Followers DATAFRAME.R
+++ Scripts/Followers DATAFRAME.R
@@ -14,3 +14,23 @@ df_fr_c <- map_dfr(follower_fr_culture, bind_rows)
 df_fr_p <- map_dfr(follower_fr_parties, bind_rows)
 df_rw_c <- map_dfr(follower_rw_culture, bind_rows)
 df_rw_p <- map_dfr(follower_rw_parties, bind_rows)
+
+# Now we export our datasets in the folders of the project
+# We use write.csv2 to have SEPARATE COLOUMNS FOR THE VALUES
+
+# RAW DATASET
+# Far right Cultures
+write.csv2(df_fr_c,"C:\\Users\\39333\\Desktop\\UNIVERSITA\\MAGISTRALE COM-DAPS&CO\\CORSI\\DATA ACCESS AND REGULATIONS\\CAPSTONE PROJECT\\Capstone_Taddei_Ita_Right\\Data\\Raw Followers DF\\RAW follower fr culture.csv", row.names=FALSE)
+
+# Far right Parties
+write.csv2(df_fr_p,"C:\\Users\\39333\\Desktop\\UNIVERSITA\\MAGISTRALE COM-DAPS&CO\\CORSI\\DATA ACCESS AND REGULATIONS\\CAPSTONE PROJECT\\Capstone_Taddei_Ita_Right\\Data\\Raw Followers DF\\RAW follower fr parties.csv", row.names=FALSE)
+
+# Right wing Cultures
+write.csv2(df_rw_c,"C:\\Users\\39333\\Desktop\\UNIVERSITA\\MAGISTRALE COM-DAPS&CO\\CORSI\\DATA ACCESS AND REGULATIONS\\CAPSTONE PROJECT\\Capstone_Taddei_Ita_Right\\Data\\Raw Followers DF\\RAW follower rw culture.csv", row.names=FALSE)
+
+# Right wing Parties
+write.csv2(df_rw_p,"C:\\Users\\39333\\Desktop\\UNIVERSITA\\MAGISTRALE COM-DAPS&CO\\CORSI\\DATA ACCESS AND REGULATIONS\\CAPSTONE PROJECT\\Capstone_Taddei_Ita_Right\\Data\\Raw Followers DF\\RAW follower rw parties.csv", row.names=FALSE)
+
+
+# CLEANING THE DATAFRAME
+
