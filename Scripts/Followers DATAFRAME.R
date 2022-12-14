166908e6521780f5f8f7bfa06b244734ca279765
federico00taddei <icotaddei@gmail.com>
2022-12-14 10:38:37 +0100

    Clean follower dataframe and export

--- Scripts/Followers DATAFRAME.R
+++ Scripts/Followers DATAFRAME.R
@@ -34,3 +34,45 @@ write.csv2(df_rw_p,"C:\\Users\\39333\\Desktop\\UNIVERSITA\\MAGISTRALE COM-DAPS&C
 
 # CLEANING THE DATAFRAME
 
+# Now we ignore the duplicates, in every dataframe. The idea is to see who are the followers of the 
+# four main groups (as they were single users with all the followers) so: Far Right Cultures and Parties,
+# Right-wing Cultures and Parties
+
+library(dplyr)
+
+# We spceify also the coloumn from_id (wich are the followers)
+
+clean_df_fr_c = df_fr_c %>% distinct(from_id, .keep_all = TRUE)
+
+clean_df_fr_p = df_fr_p %>% distinct(from_id, .keep_all = TRUE)
+
+clean_df_rw_c = df_rw_c %>% distinct(from_id, .keep_all = TRUE)
+
+clean_df_rw_p = df_rw_p %>% distinct(from_id, .keep_all = TRUE)
+
+# Now we export our datasets in the folders of the project
+# We use write.csv2 to have SEPARATE COLOUMNS FOR THE VALUES
+
+# RAW DATASET
+# Far right Cultures
+write.csv2(clean_df_fr_c,"C:\\Users\\39333\\Desktop\\UNIVERSITA\\MAGISTRALE COM-DAPS&CO\\CORSI\\DATA ACCESS AND REGULATIONS\\CAPSTONE PROJECT\\Capstone_Taddei_Ita_Right\\Data\\Clean Followers DF\\Clean follower fr culture.csv", row.names=FALSE)
+
+# Far right Parties
+write.csv2(clean_df_fr_p,"C:\\Users\\39333\\Desktop\\UNIVERSITA\\MAGISTRALE COM-DAPS&CO\\CORSI\\DATA ACCESS AND REGULATIONS\\CAPSTONE PROJECT\\Capstone_Taddei_Ita_Right\\Data\\Clean Followers DF\\Clean follower fr parties.csv", row.names=FALSE)
+
+# Right wing Cultures
+write.csv2(clean_df_rw_c,"C:\\Users\\39333\\Desktop\\UNIVERSITA\\MAGISTRALE COM-DAPS&CO\\CORSI\\DATA ACCESS AND REGULATIONS\\CAPSTONE PROJECT\\Capstone_Taddei_Ita_Right\\Data\\Clean Followers DF\\Clean follower rw culture.csv", row.names=FALSE)
+
+# Right wing Parties
+write.csv2(clean_df_rw_p,"C:\\Users\\39333\\Desktop\\UNIVERSITA\\MAGISTRALE COM-DAPS&CO\\CORSI\\DATA ACCESS AND REGULATIONS\\CAPSTONE PROJECT\\Capstone_Taddei_Ita_Right\\Data\\Clean Followers DF\\Clean follower rw parties.csv", row.names=FALSE)
+
+
+
+
+
+
+
+
+
+
+
