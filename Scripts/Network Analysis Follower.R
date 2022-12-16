ed994e2181b9e998c0bfea109d405c29c17e48dc
federico00taddei <icotaddei@gmail.com>
2022-12-16 19:22:36 +0100

    common follower df, first structure for plotting (barchart comparing common or not follower by groups)

--- Scripts/Network Analysis Follower.R
+++ Scripts/Network Analysis Follower.R
@@ -27,7 +27,7 @@ write.csv2(tot_followers,"C:\\Users\\39333\\Desktop\\UNIVERSITA\\MAGISTRALE COM-
 # We can see how many followers graphically the four groups have
 library(ggplot2)
 
-ggplot(tot_followers, aes(x = grp, )) +
+ggplot(tot_followers, aes(x = grp)) +
   geom_bar(stat="count",width=0.4, alpha = 1, fill = c("red", "black", "yellow", "darkblue"),  binwidth = 1) +
   geom_text(aes(label = after_stat(count)), stat = "count", vjust = -0.5, colour = "black") +
   xlab("Group") +
@@ -36,15 +36,45 @@ ggplot(tot_followers, aes(x = grp, )) +
   scale_y_continuous(labels = scales::comma)+
   theme_bw()
 
-############################
-# sistemare colori e numeri
-
-
 # Now we see how many common followers the four groups have
 
 unique_follower = tot_followers %>% distinct(follower_id, .keep_all = TRUE)
+colnames(unique_follower) = c("follower_id", "grp")
 
 554393-447810 # [1] 106583 common followers between the four groups
+
+# So common followers are:
+
+install.packages("data.table")
+library(data.table)
+
+# We transform tot_followers and unique_follower in data.tables
+
+dftf=data.table(tot_followers)
+dfuf=data.table(unique_follower)
+
+common_follower = fsetdiff(dftf, dfuf, all = TRUE)
+
+# We export the two dataframes of the common and the unique followers:
+
+write.csv2(unique_follower,"C:\\Users\\39333\\Desktop\\UNIVERSITA\\MAGISTRALE COM-DAPS&CO\\CORSI\\DATA ACCESS AND REGULATIONS\\CAPSTONE PROJECT\\Capstone_Taddei_Ita_Right\\Data\\Total Followers\\Unique Followers.csv", row.names=FALSE)
+
+write.csv2(common_follower,"C:\\Users\\39333\\Desktop\\UNIVERSITA\\MAGISTRALE COM-DAPS&CO\\CORSI\\DATA ACCESS AND REGULATIONS\\CAPSTONE PROJECT\\Capstone_Taddei_Ita_Right\\Data\\Total Followers\\Common Followers.csv", row.names=FALSE)
+
+# Now we prepare the dataframes for the hystogram to see the diffrences in the groups
+library(tidyverse)
+
+cfdf <- common_follower %>%
+  add_column(cmmn = 1)
+
+ufdf <- unique_follower %>%
+  add_column(cmmn = 0)
+
+total_foll_com = rbind(cfdf, ufdf)
+
+ggplot(total_foll_com) +
+  geom_bar(aes(x = grp, y = follower_id, fill = cmmn), 
+           stat="identity")
 
 
 
